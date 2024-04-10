b5b171308a54291ad4778a9be3d56a8312d0b19d
jeong <chung_junhuck@infonic.co.jp>
2024-04-10 09:23:32 +0900

    section2

--- src/main/java/com/eazybytes/EasyBankBackEndApplication.java
+++ src/main/java/com/eazybytes/EasyBankBackEndApplication.java
@@ -1,13 +1,13 @@
-package com.eazybytes.springsecuritybasic;
+package com.eazybytes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringsecuritybasicApplication {
+public class EasyBankBackEndApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringsecuritybasicApplication.class, args);
+		SpringApplication.run(EasyBankBackEndApplication.class, args);
 	}
 
 }
