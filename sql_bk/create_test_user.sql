d23249798eba1a7838d451f20d38be165ddf872e
jeong <chung_junhuck@infonic.co.jp>
2024-04-20 00:16:01 +0900

    sqls

---
+++ sql_bk/create_test_user.sql
@@ -0,0 +1,2 @@
+INSERT IGNORE INTO `users` VALUES (NULL, 'happy', '12345', '1');
+INSERT IGNORE INTO `authorities` VALUES (NULL, 'happy', 'write');
---
+++ src/main/resources/sql/scripts.sql
@@ -0,0 +1,20 @@
+create database eazybank;
+use eazybank;
+
+create table `users`(
+    `id` INT NOT NULL AUTO_INCREMENT,
+	`username` VARCHAR(50) NOT NULL,
+	`password` VARCHAR(500) NOT NULL,
+	`enabled` boolean NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+create table `authorities` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`username` varchar(50) NOT NULL,
+	`authority` varchar(50) NOT NULL,
+	primary key(`id`)
+);
+
+INSERT IGNORE INTO `users` VALUES (NULL, 'happy', '12345', '1');
+INSERT IGNORE INTO `authorities` VALUES (NULL, 'happy', 'write');
