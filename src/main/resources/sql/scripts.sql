d16ffb400ed3ec0d97eee3bd133fb7c229014b34
jeong <chung_junhuck@infonic.co.jp>
2024-04-20 14:47:37 +0900

    create custom table

--- src/main/resources/sql/scripts.sql
+++ src/main/resources/sql/scripts.sql
@@ -18,3 +18,11 @@ create table `authorities` (
 
 INSERT IGNORE INTO `users` VALUES (NULL, 'happy', '12345', '1');
 INSERT IGNORE INTO `authorities` VALUES (NULL, 'happy', 'write');
+
+CREATE TABLE `customer` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `email` varchar(45) NOT NULL,
+    `pwd` varchar(200) NOT NULL,
+    `role` varchar(45) NOT NULL,
+    PRIMARY KEY (`id`)
+);
